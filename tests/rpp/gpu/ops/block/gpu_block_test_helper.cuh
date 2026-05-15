856bcb18bd80bba40917207028e83f2c443e801e
Sam Morley <admin@inakleinbottle.com>
2026-05-15 17:57:35 +0100

    Return tagged pointers from device_data helpers and update tensor_pairing test to use them directly.

--- tests/rpp/gpu/ops/block/gpu_block_test_helper.cuh
+++ tests/rpp/gpu/ops/block/gpu_block_test_helper.cuh
@@ -177,15 +177,15 @@ struct GpuBlockTestHelper {
     }
 
     template <typename T>
-    [[nodiscard]] static T* device_data(DeviceVector<T>& data)
+    [[nodiscard]] static auto device_data(DeviceVector<T>& data)
     {
-        return thrust::raw_pointer_cast(data.data());
+        return rpp::tag_pointer<GpuArchitecture>(thrust::raw_pointer_cast(data.data()));
     }
 
     template <typename T>
-    [[nodiscard]] static T const* device_data(DeviceVector<T> const& data)
+    [[nodiscard]] static auto device_data(DeviceVector<T> const& data)
     {
-        return thrust::raw_pointer_cast(data.data());
+        return rpp::tag_pointer<GpuArchitecture>(thrust::raw_pointer_cast(data.data()));
     }
 
     template <typename T>
