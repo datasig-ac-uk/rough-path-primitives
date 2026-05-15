a930174c0ef0f0ce23a6b7580ba991136b613a0c
Sam Morley <admin@inakleinbottle.com>
2026-05-15 17:56:31 +0100

    Switch sparse matrix‑vector kernel to atomic accumulation, add CUDA atomic and algorithm includes, introduce atomic_out helper (and block_wide stub), and make set_constant private.

--- include/rpp/gpu/operations/block/basic/sparse_matrix_vector.hpp
+++ include/rpp/gpu/operations/block/basic/sparse_matrix_vector.hpp
@@ -3,10 +3,14 @@
 
 #include <cstddef>
 
+
+#include <cuda/atomic>
+
 #include <rpp/config.h>
 #include <rpp/utility.hpp>
 #include <rpp/dense/batch.hpp>
 #include <rpp/sparse/matrix.hpp>
+#include <rpp/support/algorithm.hpp>
 
 #include <rpp/operations/base_operation.hpp>
 #include <rpp/operations/basic/sparse_matrix_vector.hpp>
@@ -85,6 +89,7 @@ public:
     template<typename D, typename I, typename O>
     using GradedMatrix = sparse::GradedMatrixView<sparse::MatrixFormat::CSC, D, I, O>;
 
+private:
     VectorSetConstant<Strategy> set_constant;
 public:
     static constexpr bool is_implemented = true;
@@ -94,68 +99,92 @@ public:
         return strategy.block_size * sizeof(Accum_);
     }
 
+
+private:
+
+    using BlockAtomic = cuda::atomic_ref<Accum, cuda::thread_scope_block>;
+
+    template <typename VectorOut, typename VectorArg, typename DataIter, typename IndexIter, typename OffsetIter>
+    RPP_DEVICE void atomic_out(Context const& ctx, VectorOut& out, VectorArg const& arg, Matrix<DataIter, IndexIter, OffsetIter> const &matrix, Accum alpha) const noexcept {
+
+        set_constant(ctx, out, Accum{0});
+        ctx.sync();
+
+        const auto col_begin = arg.begin_index();
+        const auto col_end = arg.end_index();
+
+        const auto row_begin = out.begin_index();
+        const auto row_end = out.end_index();
+
+        for (Index idx=ctx.thread_rank(), end=matrix.nnz(); idx<end; idx += ctx.num_threads()) {
+            auto my_row = matrix.inner_index(idx);
+            auto my_col = algo::index_upper_bound(matrix.offsets(), Index(0), matrix.cols(), idx)-1;
+
+            const Accum mat_val { matrix.value(idx) };
+            Accum arg_val { 0 };
+            if (in_range(my_col, col_begin, col_end)) {
+                arg_val = Accum { arg[my_col] };
+            }
+            arg_val *= mat_val;
+
+            if (in_range(my_row, row_begin, row_end)) {
+                BlockAtomic out_ref { out[my_row] };
+                out_ref.fetch_add(alpha * arg_val, cuda::memory_order_relaxed);
+            }
+        }
+
+
+    }
+
+    template <typename VectorOut, typename VectorArg, typename DataIter, typename IndexIter, typename OffsetIter>
+    RPP_DEVICE static void block_wide(Context const& ctx, VectorOut& out, VectorArg const& arg, Matrix<DataIter, IndexIter, OffsetIter> const& matrix, Accum alpha) noexcept {
+
+
+        auto* scratch = ctx.template shared_memory<Accum*>();
+
+        const auto col_begin = arg.begin_index();
+        const auto col_end = arg.end_index();
+
+        const auto row_begin = out.begin_index();
+        const auto row_end = out.end_index();
+
+
+        for (Index row_block_begin=row_begin; row_block_begin < row_end; row_block_begin += ctx.num_threads()) {
+            scratch[ctx.thread_rank()] = Accum{0};
+            ctx.sync();
+
+            for (Index col=col_begin; col < col_end; ++col) {
+                auto [lower, upper] = matrix.offsets(col);
+                auto block_col_begin = algo::index_lower_bound(matrix.indices(), lower, upper, row_block_begin);
+                auto block_col_end = algo::index_lower_bound(matrix.indices(), block_col_begin, upper, row_block_begin + ctx.num_threads());
+
+                auto my_offset = block_col_begin + ctx.thread_rank();
+                if (my_offset < block_col_end) {
+                    auto my_row = matrix.inner_index(my_offset);
+                    const Accum mat_val { matrix.value(my_offset) };
+                    const Accum vec_val { out[my_row] };
+                    BlockAtomic out_ref { scratch[my_row - matrix.offset(block_col_begin)] };
+                    out_ref.fetch_add(mat_val * vec_val, cuda::memory_order_relaxed);
+                }
+            }
+
+            ctx.sync();
+            auto my_row = row_block_begin + ctx.thread_rank();
+            if (my_row < row_end) {
+                out[my_row] = alpha*scratch[ctx.thread_rank()];
+            }
+
+        }
+    }
+
+public:
     template<typename VectorOut, typename DataIter, typename IndexIter, typename OffsetsIter, typename VectorArg>
     RPP_DEVICE void operator()(Context const &ctx, VectorOut &out,
                                VectorArg const &arg,
                                Matrix<DataIter, IndexIter, OffsetsIter> const &matrix,
                                Accum alpha = Accum{1}) const noexcept {
-        set_constant(ctx, out, Accum{0});
-        ctx.sync();
-
-        auto out_it = out.begin();
-        auto arg_it = arg.begin();
-
-        const auto warp_lane = ctx.warp_lane();
-        const auto warp_idx = ctx.warp_idx();
-
-        auto *scratch = ctx.template shared_memory<Accum *>() + warp_idx * Strategy::warp_size;
-
-        for (Index block_start = 0; block_start < matrix.inner_dim(); block_start += Strategy::warp_size) {
-            scratch[warp_lane] = Accum{0};
-            ctx.sync_warp();
-
-            for (Index col = warp_idx; col < matrix.cols(); col += ctx.num_warps()) {
-                const auto begin = matrix.offset(col);
-                const auto end = matrix.offset(col + 1);
-
-                for (Index idx = begin + warp_lane; idx < end; idx += Strategy::warp_size) {
-                    const auto row = matrix.inner_index(idx);
-                    const Accum coeff{matrix.value(idx)};
-                    if (block_start <= row && row < block_start + Strategy::warp_size) {
-                        const auto index = row - block_start;
-                        const Accum arg_val{arg_it[row]};
-                        scratch[index] += coeff * arg_val;
-                    }
-                }
-            }
-
-            ctx.sync();
-
-            auto *smem = ctx.template shared_memory<Accum *>();
-            for (unsigned step = ctx.num_warps(); step > 2; step /= 2) {
-                if (warp_idx < step / 2) {
-                    const unsigned left_idx = 2 * warp_idx;
-                    const unsigned right_idx = left_idx + 1;
-
-                    Accum left_val{smem[left_idx * Strategy::warp_size + warp_lane]};
-                    Accum right_val{0};
-                    if (right_idx < step) {
-                        right_val = Accum{smem[right_idx * Strategy::warp_size + warp_lane]};
-                    }
-
-                    smem[warp_idx * Strategy::warp_size + warp_lane] = left_val + right_val;
-                }
-                ctx.sync();
-            }
-
-            if (warp_idx == 0) {
-                Accum val{smem[warp_lane]};
-                if (ctx.num_warps() > 1) {
-                    val += Accum{smem[Strategy::warp_size + warp_lane]};
-                }
-                out_it[block_start + warp_lane] = val;
-            }
-        }
+        atomic_out(ctx, out, arg, matrix, alpha);
+        // block_wide(ctx, out, arg, matrix, alpha);
     }
 };
 } // namespace rpp::ops
