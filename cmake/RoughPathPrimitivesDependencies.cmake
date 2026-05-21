92a8558071e651317fc04cc909999efccd529354
Sam Morley <admin@inakleinbottle.com>
2026-05-21 19:35:24 +0100

    Correct CUDA dependency handling: initialize availability to false and only mark component as available when CUDAToolkit is found.

--- cmake/RoughPathPrimitivesDependencies.cmake
+++ cmake/RoughPathPrimitivesDependencies.cmake
@@ -9,24 +9,24 @@ macro(_rpp_check_cuda_dependencies _FPN _CPT)
         set(${_FPN}_NOT_FOUND_MESSAGE
                 "${_FPN}: ${_CPT} component requires the CUDA Compiler"
         )
-        return()
-    endif()
+    else ()
+        find_package(CUDAToolkit CONFIG QUIET)
+        if (NOT CUDAToolkit_FOUND)
+            set(${_FPN}_${_CPT}_AVAILABLE FALSE)
+            set(${_FPN}_NOT_FOUND_MESSAGE
+                    "${_FPN}: ${_CPT} component requires the CUDAToolkit"
+            )
+        else ()
+            set(${_FPN}_${_CPT}_AVAILABLE TRUE)
+        endif ()
+    endif ()
 
-    find_package(CUDAToolkit CONFIG QUIET)
-    if (NOT CUDAToolkit_FOUND)
-        set(${_FPN}_${_CPT}_AVAILABLE FALSE)
-        set(${_FPN}_NOT_FOUND_MESSAGE
-                "${_FPN}: ${_CPT} component requires the CUDAToolkit"
-        )
-        return()
-    endif()
-
-    set(${_FPN}_${_CPT}_AVAILABLE TRUE)
 endmacro()
 
 
 macro(_rpp_check_dependencies _FPN _CPT)
     if (${_CPT} MATCHES "CUDA|GPU")
+        set(${_FPN}_${_CPT}_AVAILABLE FALSE)
         _rpp_check_cuda_dependencies(${_FPN} ${_CPT})
     endif()
 endmacro()
