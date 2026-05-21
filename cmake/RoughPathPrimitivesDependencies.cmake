5cb53e8e4b08fd81299288d2b14fd40d11987e41
Sam Morley <admin@inakleinbottle.com>
2026-05-21 19:52:00 +0100

    Simplify CUDA dependency handling: check for CUDA::cudart target and set component availability only when the CUDAToolkit is found.

--- cmake/RoughPathPrimitivesDependencies.cmake
+++ cmake/RoughPathPrimitivesDependencies.cmake
@@ -4,20 +4,17 @@ include(CMakeFindDependencyMacro)
 
 
 macro(_rpp_check_cuda_dependencies _FPN _CPT)
-    if (NOT DEFINED CMAKE_CUDA_COMPILER)
-        set(${_FPN}_${_CPT}_AVAILABLE FALSE)
-        set(${_FPN}_NOT_FOUND_MESSAGE
-                "${_FPN}: ${_CPT} component requires the CUDA Compiler"
-        )
+    if (TARGET CUDA::cudart)
+        set(${_FPN}_${_CPT}_AVAILABLE TRUE)
     else ()
-        find_package(CUDAToolkit CONFIG QUIET)
-        if (NOT CUDAToolkit_FOUND)
+        find_package(CUDAToolkit QUIET)
+        if (TARGET CUDA::cudart)
+            set(${_FPN}_${_CPT}_AVAILABLE TRUE)
+        else ()
             set(${_FPN}_${_CPT}_AVAILABLE FALSE)
             set(${_FPN}_NOT_FOUND_MESSAGE
                     "${_FPN}: ${_CPT} component requires the CUDAToolkit"
             )
-        else ()
-            set(${_FPN}_${_CPT}_AVAILABLE TRUE)
         endif ()
     endif ()
 
