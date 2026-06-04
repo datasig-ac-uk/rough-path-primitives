0d2029ada11f0b91f6166614a1e58dc466328d06
Sam Morley <admin@inakleinbottle.com>
2026-06-04 16:20:16 +0100

    Expose type aliases as public members in vector_add and vector_scalar_multiply GPU block operations and tidy access specifiers.

--- include/rpp/gpu/block/operations/linalg/vector_scalar_multiply.hpp
+++ include/rpp/gpu/block/operations/linalg/vector_scalar_multiply.hpp
@@ -23,13 +23,12 @@ class VectorScalarMultiply<
     : public BaseOperation<
           gpu::strategies::
               BlockStrategy<Accum_, BlockSize, MaxBlockSize, Architecture>> {
+public:
     using Strategy = gpu::strategies::
         BlockStrategy<Accum_, BlockSize, MaxBlockSize, Architecture>;
     using Context = typename Strategy::Context;
     using Accum = typename Strategy::Accum;
     using Index = typename Strategy::Index;
-
-public:
     static constexpr bool is_implemented = true;
 
     template <typename Vector>
